7ccdca4b7995faec2bfd11de20257aabe9c08347
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-10-17 21:42:17 +0200

    Package update
    Added vue-global-events package

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
+import { GlobalEvents } from 'vue-global-events'
 import store from './store'
 import router from './router'
 import App from './App.vue'
 import './index.css'
 
 createApp(App)
+    .component('GlobalEvents', GlobalEvents)
     .use(store)
     .use(router)
     .mount('#app')
