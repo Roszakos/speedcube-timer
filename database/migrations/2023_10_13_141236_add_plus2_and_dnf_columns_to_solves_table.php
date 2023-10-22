277950bf1093c0bad9861fca1a35c3e81586b258
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-10-22 16:43:00 +0200

    Create 2023_10_13_141236_add_plus2_and_dnf_columns_to_solves_table.php
    Added plus2 and dnf columns in solves table

---
+++ database/migrations/2023_10_13_141236_add_plus2_and_dnf_columns_to_solves_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Session;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('solves', function (Blueprint $table) {
+            $table->boolean('dnf')->after('time');
+            $table->boolean('plus2')->after('time');
+        });
+    }
+};
