79aa5bf52a30fd6ba8d9a360efd29f100ae45b65
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-10-25 19:53:19 +0200

    Create SolveDetailsModal.vue
    Created modal that shows details about a specific solve

---
+++ vue/src/components/modals/SolveDetailsModal.vue
@@ -0,0 +1,64 @@
+<template>
+  <div v-if="showModal" @click="closeModal" class="fixed left-0 top-0 bg-gray-500/70 w-full h-full z-[1] shadow-lg">
+    <div @click.stop class="w-[40%] h-[25%] p-5 m-auto bg-white mt-[15rem] text-center rounded-md">
+      <div v-html="content" class="m-auto font-bold mt-5">
+
+      </div>
+      <button @click="closeModal" class="bg-sky-600 px-3 py-2 font-semibold mt-7 rounded-lg text-white ">Close
+        Details</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import store from '../../store';
+
+const content = ref(null)
+const showModal = ref(false)
+const times = computed(() => store.state.session.times)
+
+defineExpose({
+  showSolveDetails
+})
+
+function showSolveDetails(index) {
+  displayContent(index)
+    .then(() => {
+      showModal.value = true
+    })
+}
+async function displayContent(index) {
+  let newContent = '<div class="inline-block font-bold text-5xl my-3">' + displayTime(store.state.session.times[index].time) + '</div>'
+  newContent += '<div class="font-semibold">' + store.state.session.times[index].scramble + '</div>'
+  content.value = newContent
+}
+
+function closeModal() {
+  showModal.value = false
+}
+
+function displayTime(miliseconds) {
+  let secondColon = ''
+  let firstColon = ''
+
+  let seconds = ((miliseconds) / 1000)
+  let minutes = (parseInt(seconds / 60) > 0) ? parseInt(seconds / 60) : ''
+  let hours = (parseInt(minutes / 60) > 0) ? parseInt(minutes / 60) : ''
+
+  seconds = (seconds - minutes * 60).toFixed(2)
+
+  if (minutes) {
+    minutes = minutes - hours * 60
+    secondColon = ':'
+  }
+
+  if (hours) {
+    firstColon = ':'
+  }
+
+  return hours + firstColon + minutes + secondColon + seconds
+}
+</script>
+
+
