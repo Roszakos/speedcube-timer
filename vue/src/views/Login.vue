a1f093398204c0c95dac0242d3fb2844dd327eaa
Roszakos <139218017+Roszakos@users.noreply.github.com>
2023-11-02 20:29:44 +0100

    Add loading animation
    Added loading animation when sign in and sign up buttons are clicked

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -31,13 +31,22 @@
       </div>
 
       <div>
-        <button type="submit"
-          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-          in</button>
+        <button type="submit" :disabled="loading" :class="[loading ? 'cursor-not-allowed' : '']"
+          class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+
+          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+          </svg>
+          Sign in
+        </button>
       </div>
     </form>
 
-    <p class="mt-10 text-center text-sm text-gray-500">
+    <p class="mt-10 text-center text-sm text-gray-700">
       Don't have an account yet?
       <router-link :to="{ name: 'Register' }" class="text-indigo-700 font-semibold">Sign up for free</router-link>
     </p>
@@ -58,10 +67,19 @@ const data = ref({
   password: ''
 })
 
+const loading = ref(false)
+
 function login() {
+  loading.value = true
   store.dispatch('login', data.value)
     .then(() => {
-      router.push('/')
+      loading.value = false
+      router.push({
+        name: 'Timer'
+      })
+    })
+    .catch(err => {
+      loading.value = false
     })
 }
 
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -50,9 +50,17 @@
       </div>
 
       <div>
-        <button type="submit"
-          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-          in</button>
+        <button type="submit" :disabled="loading" :class="[loading ? 'cursor-not-allowed' : '']"
+          class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+          </svg>
+          Sign up
+        </button>
       </div>
     </form>
 
@@ -72,6 +80,8 @@ import { ref } from 'vue'
 const store = useStore()
 const router = useRouter()
 
+const loading = ref(false)
+
 const data = ref({
   nickname: '',
   email: '',
@@ -80,9 +90,16 @@ const data = ref({
 })
 
 function register() {
+  loading.value = true
   store.dispatch('register', data.value)
     .then(() => {
-      router.push('/')
+      loading.value = false
+      router.push({
+        name: 'Timer'
+      })
+    })
+    .catch(err => {
+      loading.value = false
     })
 }
 </script>
